dcfb05e77a6647557bb4bda8f4b0bb0768f24dbd
Craig Bartholomew <craigbartjr@yahoo.com>
2018-08-03 00:07:18 -0400

    Added facebook login

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,13 +1,19 @@
-import { firebase, googleAuthProvider } from '../firebase/firebase';
+import { firebase, googleAuthProvider, facebookAuthProvider } from '../firebase/firebase';
 
 export const login = (uid) => ({
     type: 'LOGIN',
     uid
 });
 
-export const startLogin = () => {
+export const googleLogin = () => {
     return () => {
         return firebase.auth().signInWithPopup(googleAuthProvider);
+    };
+};
+
+export const facebookLogin = () => {
+    return () => {
+        return firebase.auth().signInWithPopup(facebookAuthProvider);
     };
 };
 
--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,19 +1,21 @@
 import React from "react";
 import { connect } from 'react-redux';
-import { startLogin } from '../actions/auth';
+import { googleLogin, facebookLogin } from '../actions/auth';
 
-export const LoginPage = ({ startLogin }) => (
+export const LoginPage = ({ googleLogin, facebookLogin }) => (
     <div className="box-layout">
         <div className="box-layout__box">
             <h1 className="box-layout__title">Expensify</h1>
             <p>It's time to get your expenses under control</p>
-            <button className="button" onClick={startLogin}>Login with Google</button>
+            <button className="button" onClick={googleLogin}>Login with Google</button>
+            <button className="button" onClick={facebookLogin}>Login with Facebook</button>
         </div>
     </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
-    startLogin: () => dispatch(startLogin())
+    googleLogin: () => dispatch(googleLogin()),
+    facebookLogin: () => dispatch(facebookLogin())
 });
 
 export default connect(undefined, mapDispatchToProps)(LoginPage);
